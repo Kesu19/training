8119669b3a5a2cfe0c57fe35a7d4441082c92d22
Kelvin SUDROT <kelvin.sudrot@greenwishes.fr>
2021-10-22 13:04:44 +0200

    Update first-crud.service.ts

--- frontend/src/app/services/firstCrudService/first-crud.service.ts
+++ frontend/src/app/services/firstCrudService/first-crud.service.ts
@@ -1,11 +1,11 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders, HttpParams  } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 const optionRequete = {
-  headers: new HttpHeaders({ 
-    'Access-Control-Allow-Origin':'*',
-    'mon-entete-personnalise':'maValeur'
+  headers: new HttpHeaders({
+    'Access-Control-Allow-Origin': '*',
+    'mon-entete-personnalise': 'maValeur'
   })
 };
 
@@ -15,23 +15,25 @@ const optionRequete = {
 export class FirstCrudService {
 
   constructor(private http: HttpClient) { }
-  
-  getAllUser(): Observable<any[]>{
+
+  getAllUser(): Observable<any[]> {
     const endpoint = 'http://localhost:4000/crud/selectAllUser';
     return this.http.get<any[]>(endpoint);
   }
-  
-  deletUser(id: any): Observable<any>{
+
+  deletUser(id: any): Observable<any> {
     let params = new HttpParams().set('id', id);
     const endpoint = `http://localhost:4000/crud/deletUser`;
     console.log(params)
-    return this.http.get<any>(endpoint,{ params: params })
+    return this.http.get<any>(endpoint, { params: params })
 
   }
-  updateUser(id:any):Observable<any>{
-    let params = new HttpParams().set('id',id)
-    const endpoint = `http://localhost:4000/crud/deletUser`;
-    console.log(params)
-    return this.http.get<any>(endpoint,{params:params})
+  updateUser(id: any, newPassword: any): Observable<any> {
+    let parametres = new HttpParams();
+    parametres = parametres.append('id', id);
+    parametres = parametres.append('mdp', newPassword);
+    const endpoint = `http://localhost:4000/crud/updateUser`;
+    console.log(parametres)
+    return this.http.get<any>(endpoint, { params: parametres })
   }
 }
