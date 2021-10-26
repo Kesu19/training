8cd4b0a5c34eb30ee9beac841da68c6321d3df7e
Kelvin SUDROT <kelvin.sudrot@greenwishes.fr>
2021-10-26 10:23:38 +0200

    Update first-crud.service.ts

--- frontend/src/app/services/firstCrudService/first-crud.service.ts
+++ frontend/src/app/services/firstCrudService/first-crud.service.ts
@@ -1,11 +1,10 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
-
 const optionRequete = {
-  headers: new HttpHeaders({
-    'Access-Control-Allow-Origin': '*',
-    'mon-entete-personnalise': 'maValeur'
+  headers: new HttpHeaders({ 
+    'Access-Control-Allow-Origin':'*',
+    'mon-entete-personnalise':'maValeur'
   })
 };
 
@@ -35,5 +34,15 @@ export class FirstCrudService {
     const endpoint = `http://localhost:4000/crud/updateUser`;
     console.log(parametres)
     return this.http.get<any>(endpoint, { params: parametres })
+  }
+  insertUser(nom :any,prenom:any,login:any, newPassword: any): Observable<any> {
+    let parametres = new HttpParams();
+    parametres = parametres.append('nom', nom);
+    parametres = parametres.append('prenom', prenom);
+    parametres = parametres.append('nom', login);
+    parametres = parametres.append('mdp', newPassword);
+    const endpoint = `http://localhost:4000/crud/insertUser`;
+    console.log(parametres)
+    return this.http.post<any>(endpoint,{ nom, prenom, login, newPassword},optionRequete)
   }
 }
