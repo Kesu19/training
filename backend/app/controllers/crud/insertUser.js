8f4209ee6027f3d972ae85cd6b8130506f12d25f
Kelvin SUDROT <kelvin.sudrot@greenwishes.fr>
2021-10-26 11:47:29 +0200

    Update insertUser.js

--- backend/app/controllers/crud/insertUser.js
+++ backend/app/controllers/crud/insertUser.js
@@ -10,15 +10,15 @@ module.exports = class insertUser {
     }
 
     async middleware() {
-        this.app.post(`/crud/insertUser`, async (req, res) => {
-                const pool = await poolPromise
-                console.log(req.body)
-                const newUser = `insert INTO dbo.utilisateurs(nom, prenom, login, mdp) values('${req.body.nom}','${req.body.prenom}','${req.body.login}','${req.body.mdp}')`
-                console.log(newUser);
-                const ajouter = await pool.request().query(newUser)
+        this.app.post(`/crud/insertUser`,cors(), async (req, res) => {
+            const pool = await poolPromise
+            console.log(req.body)
+            const newUser = `insert INTO dbo.utilisateurs(nom, prenom, login, mdp) values('${req.body.nom}','${req.body.prenom}','${req.body.login}','${req.body.mdp}')`
+            console.log(newUser);
+            const ajouter = await pool.request().query(newUser)
             res.status(200).json(ajouter)
         })
-        
+
     }
 
 
